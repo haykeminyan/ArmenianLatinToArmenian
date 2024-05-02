f144a3f43753d3af34862b075da11ecc47832665
Hayk Eminyan <43892880+haykeminyan@users.noreply.github.com>
2024-05-03 02:58:39 +0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="armenian_latin_to_armenian_hy",
+	version="0.0.1",
+	description="Transliteration from Latin Armenian language to Armenian",
+	packages=find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires=">=3.6",
+)
